96dd4bafb0784434204f5dd06e5a3bac0e80adb1
mahimgolder <mahimgolder@mahim>
2024-11-06 23:24:09 +0600

    Added all file & Created fake data for the Blogs & Local data

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <div>
+
+            <h1 className='text-4xl font-bold'>Knowlende Cafe</h1>
+            
+        </div>
+    );
+};
+
+export default Header;
